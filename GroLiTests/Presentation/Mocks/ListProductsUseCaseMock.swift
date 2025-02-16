03f9533b98f0bc9d6e4c3658e22cb4e91655e322
Nils Ramsperger <nils@ramsperger-home.de>
2025-02-16 10:50:52 +0100

    Added tests for the ShoppingListViewModel

---
+++ GroLiTests/Presentation/Mocks/ListProductsUseCaseMock.swift
@@ -0,0 +1,8 @@
+//
+//  ListProductsUseCaseMock.swift
+//  GroLiTests
+//
+//  Created by Nils Ramsperger on 16.02.25.
+//
+
+import Foundation
---
+++ GroLiTests/Presentation/ViewModels/ShoppingListViewModelTest.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingListViewModelTest.swift
+//  GroLiTests
+//
+//  Created by Nils Ramsperger on 16.02.25.
+//
+
+import Foundation
